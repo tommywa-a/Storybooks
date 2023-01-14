e0ba1d8e48a2682903a749638c3772f244530634
tommywa-a <tomiwa95@gmail.com>
2023-01-14 23:10:18 +0100

    Fixed issue with db connection and app crash

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path: './config/config.env' })
+
+connectDB()
 
 const app = express()
 
--- config/db.js
+++ config/db.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn =  await mongoose.connect(process.nextTick.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false
+      // useFindAndModify: false,
     })
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(error)
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (err) {
+    console.error(err)
     process.exit(1)
   }
 }
