f1beba6580ea976cc010484f98221e7505443bff
tommywa-a <tomiwa95@gmail.com>
2023-01-15 23:43:12 +0100

    Setting uo Morgan and Handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 // Load config
@@ -9,6 +10,10 @@ dotenv.config({ path: './config/config.env' })
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 const PORT = process.env.PORT || 3000
 
