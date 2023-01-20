066b15f0c58c72338f98918a1b3366d19002e42b
tommywa-a <tomiwa95@gmail.com>
2023-01-20 14:47:07 +0100

    Setting Static Folder and Login Layout

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
@@ -19,6 +20,9 @@ if (process.env.NODE_ENV === 'development') {
 // Handlebars
 app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
