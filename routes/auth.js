1ae05cc9a983416832ab852b8c2e3edb2bd6048d
tommywa-a <tomiwa95@gmail.com>
2023-01-24 22:34:20 +0100

    Logout feature

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,15 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 router.get('/google/callback', passport.authenticate('google', {
   failureRedirect: '/'
 }), (req, res) => {
-  res.redirect('dashoard')
+  res.redirect('/dashoard')
+}
+)
+
+// @desc Logout user
+// @route /auth/logout
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
 })
 
 module.exports = router
