122ae556b6567889823eaabd503297818dc2c555
tommywa-a <tomiwa95@gmail.com>
2023-02-04 21:46:09 +0100

    Edit Story

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Handlebars Helpers
-const { formatDate, stripTags, truncate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs')
 
 // Handlebars
-app.engine('.hbs', exphbs.engine({ helpers: {formatDate, stripTags, truncate,}, defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs.engine({ helpers: {formatDate, stripTags, truncate, editIcon, }, defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -44,6 +44,12 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
 
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
