a0622384d3918745cd0aed32671366e114e4b3b4
tommywa-a <tomiwa95@gmail.com>
2023-02-08 11:57:17 +0100

    Single Story Page

--- routes/stories.js
+++ routes/stories.js
@@ -40,6 +40,27 @@ router.get('/', ensureAuth, async (req, res) => {
   }
 })
 
+// @desc  Show Single Story
+// @route GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id)
+      .populate('user')
+      .lean()
+
+      if (!story) {
+        return res.render('error/404')
+      }
+
+      res.render('stories/show', {
+        story
+      })
+  } catch (err) {
+    console.error(err)
+    res.render('error/404')
+  }
+})
+
 // @desc  Show Edit Page
 // @route GET /stories/edit/id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
